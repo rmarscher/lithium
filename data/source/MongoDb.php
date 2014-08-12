72f122a8dd47a7007fda810f1aee177440a6ab80
Rob Marscher <rob@robmarscher.com>
2014-08-12 09:16:44 -0400

    Updated deprecated Mongo extension components.
    Tested with pecl-mongo 1.5.5.

--- data/source/MongoDb.php
+++ data/source/MongoDb.php
@@ -8,7 +8,7 @@
 
 namespace lithium\data\source;
 
-use Mongo;
+use MongoClient;
 use MongoId;
 use MongoCode;
 use MongoDate;
@@ -122,23 +122,24 @@ class MongoDb extends \lithium\data\Source {
 	 *
 	 * @see lithium\data\Connections::add()
 	 * @see lithium\data\source\MongoDb::$_schema
-	 * @link http://php.net/manual/en/mongo.construct.php PHP Manual: Mongo::__construct()
+	 * @link http://php.net/manual/en/mongo.construct.php PHP Manual: MongoClient::__construct()
 	 * @param array $config All information required to connect to the database, including:
 	 *        - `'database'` _string_: The name of the database to connect to. Defaults to `null`.
 	 *        - `'host'` _string_: The IP or machine name where Mongo is running, followed by a
 	 *           colon, and the port number. Defaults to `'localhost:27017'`.
 	 *        - `'persistent'` _mixed_: Determines a persistent connection to attach to. See the
 	 *           `$options` parameter of
-	 *            [`Mongo::__construct()`](http://www.php.net/manual/en/mongo.construct.php) for
-	 *            more information. Defaults to `false`, meaning no persistent connection is made.
+	 *            [`MongoClient::__construct()`](http://www.php.net/manual/en/mongo.construct.php)
+	 *            for more information. Defaults to `false`, meaning no persistent connection is
+	 *            made.
 	 *        - `'timeout'` _integer_: The number of milliseconds a connection attempt will wait
 	 *          before timing out and throwing an exception. Defaults to `100`.
 	 *        - `'schema'` _closure_: A closure or anonymous function which returns the schema
 	 *          information for a model class. See the `$_schema` property for more information.
 	 *        - `'gridPrefix'` _string_: The default prefix for MongoDB's `chunks` and `files`
 	 *          collections. Defaults to `'fs'`.
-	 *        - `'replicaSet'` _boolean_: See the documentation for `Mongo::__construct()`. Defaults
-	 *          to `false`.
+	 *        - `'replicaSet'` _boolean_: See the documentation for `MongoClient::__construct()`.
+	 *        	Defaults to `false`.
 	 *
 	 * Typically, these parameters are set in `Connections::add()`, when adding the adapter to the
 	 * list of active connections.
@@ -148,7 +149,7 @@ class MongoDb extends \lithium\data\Source {
 			'persistent' => false,
 			'login'      => null,
 			'password'   => null,
-			'host'       => Mongo::DEFAULT_HOST . ':' . Mongo::DEFAULT_PORT,
+			'host'       => MongoClient::DEFAULT_HOST . ':' . MongoClient::DEFAULT_PORT,
 			'database'   => null,
 			'timeout'    => 100,
 			'replicaSet' => false,
@@ -238,7 +239,7 @@ class MongoDb extends \lithium\data\Source {
 	 * database connection.
 	 *
 	 * @see lithium\data\source\MongoDb::__construct()
-	 * @link http://php.net/manual/en/mongo.construct.php PHP Manual: Mongo::__construct()
+	 * @link http://php.net/manual/en/mongo.construct.php PHP Manual: MongoClient::__construct()
 	 * @return boolean Returns `true` the connection attempt was successful, otherwise `false`.
 	 */
 	public function connect() {
@@ -260,7 +261,7 @@ class MongoDb extends \lithium\data\Source {
 			if ($persist = $cfg['persistent']) {
 				$options['persist'] = $persist === true ? 'default' : $persist;
 			}
-			$this->server = new Mongo($connection, $options);
+			$this->server = new MongoClient($connection, $options);
 
 			if ($this->connection = $this->server->{$cfg['database']}) {
 				$this->_isConnected = true;
@@ -281,7 +282,7 @@ class MongoDb extends \lithium\data\Source {
 	 * @return boolean True
 	 */
 	public function disconnect() {
-		if ($this->server && $this->server->connected) {
+		if ($this->server && $this->server->getConnections() != array()) {
 			$this->_isConnected = false;
 			unset($this->connection, $this->server);
 		}
@@ -372,7 +373,7 @@ class MongoDb extends \lithium\data\Source {
 	 * @filter
 	 */
 	public function create($query, array $options = array()) {
-		$defaults = array('safe' => false, 'fsync' => false);
+		$defaults = array('w' => 1, 'fsync' => false);
 		$options += $defaults;
 		$this->_checkConnection();
 
@@ -505,7 +506,7 @@ class MongoDb extends \lithium\data\Source {
 	 * @filter
 	 */
 	public function update($query, array $options = array()) {
-		$defaults = array('upsert' => false, 'multiple' => true, 'safe' => false, 'fsync' => false);
+		$defaults = array('upsert' => false, 'multiple' => true, 'w' => 1, 'fsync' => false);
 		$options += $defaults;
 		$this->_checkConnection();
 
@@ -550,7 +551,7 @@ class MongoDb extends \lithium\data\Source {
 	 */
 	public function delete($query, array $options = array()) {
 		$this->_checkConnection();
-		$defaults = array('justOne' => false, 'safe' => false, 'fsync' => false);
+		$defaults = array('justOne' => false, 'w' => 1, 'fsync' => false);
 		$options = array_intersect_key($options + $defaults, $defaults);
 		$_config = $this->_config;
 		$params = compact('query', 'options');
@@ -570,7 +571,7 @@ class MongoDb extends \lithium\data\Source {
 	}
 
 	protected function _deleteFile($conditions, $options = array()) {
-		$defaults = array('safe' => true);
+		$defaults = array('w' => 1);
 		$options += $defaults;
 
 		$grid = $this->connection->getGridFS();
